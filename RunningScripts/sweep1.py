465bb19716b79749dcf9dbac84803309e3cf6060
shalom2552 <shalom2552@gmail.com>
2024-09-02 15:59:21 +0300

    sweep changes

---
+++ RunningScripts/sweep1.py
@@ -0,0 +1,34 @@
+import wandb
+YOUR_WANDB_USERNAME = "shalom_and_amit"
+project = "NLP2024_PROJECT_206320772_313510679"
+
+command = [
+        "${ENVIRONMENT_VARIABLE}",
+        "${interpreter}",
+        "StrategyTransfer.py",
+        "${project}",
+        "${args}"
+    ]
+
+sweep_config = {
+    "name": "sweep_1",
+    "method": "grid",
+    "metric": {
+        "goal": "maximize",
+        "name": "AUC.test.max"
+    },
+    "parameters": {
+    	"total_epochs" : {"values": [1]},
+        "ENV_HPT_mode": {"values": [False]},
+        "architecture": {"values": ["LSTM"]},
+        "seed": {"values": list(range(1, 6))},
+        "online_simulation_factor": {"values": [0, 4]},
+        "features": {"values": ["EFs", "GPT4", "BERT"]},
+    },
+    "command": command
+}
+
+# Initialize a new sweep
+sweep_id = wandb.sweep(sweep=sweep_config, project=project)
+print("run this line to run your agent in a screen:")
+print(f"screen -dmS \"sweep_agent\" wandb agent {YOUR_WANDB_USERNAME}/{project}/{sweep_id}")
